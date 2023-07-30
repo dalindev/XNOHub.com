c0b4a239df1f6815bda8dfce971133057ec88bf9
dalindev <dhuan023@gmail.com>
2023-07-30 02:39:18 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import { Card, Title, Text } from '@tremor/react';
+import { queryBuilder } from '../lib/planetscale';
+import Search from './search';
+import UsersTable from './table';
+
+export const dynamic = 'force-dynamic';
+
+export default async function IndexPage({
+  searchParams
+}: {
+  searchParams: { q: string };
+}) {
+  const search = searchParams.q ?? '';
+  const users = await queryBuilder
+    .selectFrom('users')
+    .select(['id', 'name', 'username', 'email'])
+    .where('name', 'like', `%${search}%`)
+    .execute();
+
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Title>Users</Title>
+      <Text>
+        A list of users retrieved from a MySQL database (PlanetScale).
+      </Text>
+      <Search />
+      <Card className="mt-6">
+        <UsersTable users={users} />
+      </Card>
+    </main>
+  );
+}
