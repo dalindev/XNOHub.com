0a82c21d40e3d1334e915ad246746d98838eb0ae
DalinHuang <dhuan023@gmail.com>
2023-08-01 23:12:58 -0400

    coming soon page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,12 @@ import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import Nav from './nav';
-import Toast from './toast';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'Next.js 13 + PlanetScale + NextAuth + Tailwind CSS',
+  title: 'Nano Network Visualizer - Coming Soon',
   description:
-    'A user admin dashboard configured with Next.js, PlanetScale, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.'
+    'Stay tuned for the Nano Network Visualizer, an interactive tool to explore the entire Nano cryptocurrency network.'
 };
 
 export default async function RootLayout({
@@ -17,14 +16,22 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full bg-gray-50">
-      <body className="h-full">
-        <Suspense>
+    <html lang="en" className="h-full bg-gradient-to-r from-black to-gray-800">
+      <body className="h-full text-white">
+        {/* <Suspense>
           <Nav />
-        </Suspense>
-        {children}
+        </Suspense> */}
+        {/* {children} */}
+        <main className="flex flex-col items-center justify-center h-screen text-2xl text-center">
+          <h1 className="mb-4 text-orange-500 text-4xl">
+            Our XNOHub.com App is Coming Soon!
+          </h1>
+          <p className="text-2xl text-blue-300">
+            "What's a Bitcoin enthusiast's favorite drink? Feeeeees!" LOL
+          </p>
+          <p className="text-2xl">We're working hard Nano fam, Stay tuned!</p>
+        </main>
         <Analytics />
-        <Toast />
       </body>
     </html>
   );
