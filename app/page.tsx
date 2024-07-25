c6a1f1af8872759b1903b748b136d146d99cf161
DalinHuang <dhuan023@gmail.com>
2024-07-25 02:39:00 -0400

    added loader

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@
 import { Suspense } from 'react';
 import ThreeSceneClientWrapper from '@/components/three-scene-client-wrapper';
 import { ConfirmationProvider } from '@/providers/confirmation-provider';
+import Loader from '@/components/loading';
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <ConfirmationProvider>
         <ThreeSceneClientWrapper />
       </ConfirmationProvider>
