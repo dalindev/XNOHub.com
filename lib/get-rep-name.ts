aba1b3dd23b530e42f9763b1306b3e5faa14c4d5
DalinHuang <dhuan023@gmail.com>
2024-07-30 00:24:22 -0400

    improve representiative displayed name

--- lib/get-rep-name.ts
+++ lib/get-rep-name.ts
@@ -1,6 +1,6 @@
 import { RepsData } from '@/data/defualtMergedRepsData';
 
-export const getRepName = (account: string): string => {
+export const getRepName = (account: string) => {
   const rep = RepsData.find((rep) => rep.account === account);
-  return rep ? rep.account_formatted : 'Rep name unknown';
+  return rep ? rep.account_formatted : null;
 };
