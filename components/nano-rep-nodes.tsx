8d2a16cb8cd508b42f113580036aae2778d67a1d
DalinHuang <dhuan023@gmail.com>
2024-09-20 21:33:22 -0400

    improve node layer performance

--- components/nano-rep-nodes.tsx
+++ components/nano-rep-nodes.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useCallback, useRef } from 'react';
 import * as THREE from 'three';
 import { IRepData } from '@/types/index';
 import NetworkArcs from './network-arc';
@@ -10,43 +10,62 @@ interface NanoRepNodesProps {
   onNodeClick: (nodeRepsGeoInfo: IRepData) => void;
 }
 
-const NanoRepNodes: React.FC<NanoRepNodesProps> = ({
-  repsGeoInfo,
-  earthRadius,
-  onNodeHover,
-  onNodeClick
-}) => {
-  const nodes = useMemo(() => {
-    return repsGeoInfo.map((rep) => ({
-      ...rep,
-      position: calculatePosition(rep.latitude, rep.longitude, earthRadius),
-      color: new THREE.Color(Math.random(), Math.random(), Math.random())
-    }));
-  }, [repsGeoInfo, earthRadius]);
+const NanoRepNodes: React.FC<NanoRepNodesProps> = React.memo(
+  ({ repsGeoInfo, earthRadius, onNodeHover, onNodeClick }) => {
+    const earthRadiusRef = useRef(earthRadius);
 
-  return (
-    <group>
-      {nodes.map((node, index) => (
-        <Node
-          key={index}
-          node={node}
-          earthRadius={earthRadius}
-          onHover={onNodeHover}
-          onClick={onNodeClick}
-        />
-      ))}
-      <NetworkArcs nodes={nodes} earthRadius={earthRadius} />
-    </group>
-  );
-};
+    const nodes = useMemo(() => {
+      return repsGeoInfo.map((rep) => ({
+        ...rep,
+        position: calculatePosition(
+          rep.latitude,
+          rep.longitude,
+          earthRadiusRef.current
+        ),
+        color: new THREE.Color(0x1a6dd4)
+      }));
+    }, [repsGeoInfo]);
+
+    const handleNodeHover = useCallback(
+      (node: IRepData | null) => {
+        onNodeHover(node);
+      },
+      [onNodeHover]
+    );
+
+    const handleNodeClick = useCallback(
+      (node: IRepData) => {
+        onNodeClick(node);
+      },
+      [onNodeClick]
+    );
+
+    return (
+      <group>
+        {nodes.map((node, index) => (
+          <Node
+            key={node.account}
+            node={node}
+            earthRadius={earthRadiusRef.current}
+            onHover={handleNodeHover}
+            onClick={handleNodeClick}
+          />
+        ))}
+        <NetworkArcs nodes={nodes} earthRadius={earthRadiusRef.current} />
+      </group>
+    );
+  }
+);
+
+NanoRepNodes.displayName = 'NanoRepNodes';
 
 const calculatePosition = (
-  latitude: number,
-  longitude: number,
+  lat: number,
+  long: number,
   radius: number
 ): THREE.Vector3 => {
-  const phi = (90 - latitude) * (Math.PI / 180);
-  const theta = (longitude + 180) * (Math.PI / 180);
+  const phi = (90 - lat) * (Math.PI / 180);
+  const theta = (long + 180) * (Math.PI / 180);
   const x = -radius * Math.sin(phi) * Math.cos(theta);
   const y = radius * Math.cos(phi);
   const z = radius * Math.sin(phi) * Math.sin(theta);
@@ -60,67 +79,82 @@ interface NodeProps {
   onClick: (nodeRepsGeoInfo: IRepData) => void;
 }
 
-const Node: React.FC<NodeProps> = ({ node, earthRadius, onHover, onClick }) => {
-  const [hovered, setHovered] = useState(false);
+const Node: React.FC<NodeProps> = React.memo(
+  ({ node, earthRadius, onHover, onClick }) => {
+    const [hovered, setHovered] = React.useState(false);
 
-  const barHeight = useMemo(() => {
-    const baseHeight = earthRadius * 0.2; // 20% of Earth's radius as base height
-    const variableHeight = (node.weight_percent / 50) * earthRadius;
-    const hoverMultiplier = hovered ? 1.3 : 1; // Increase height by 30% when hovered
-    return (baseHeight + variableHeight) * hoverMultiplier;
-  }, [earthRadius, node.weight_percent, hovered]);
+    const barHeight = useMemo(() => {
+      const baseHeight = earthRadius * 0.2;
+      const variableHeight = (node.weight_percent / 50) * earthRadius;
+      const hoverMultiplier = hovered ? 1.3 : 1;
+      return (baseHeight + variableHeight) * hoverMultiplier;
+    }, [earthRadius, node.weight_percent, hovered]);
 
-  const barGeometry = useMemo(() => {
-    const geometry = new THREE.CylinderGeometry(0.0005, 0.006, barHeight, 32);
-    geometry.translate(0, barHeight / 2, 0);
-    return geometry;
-  }, [barHeight]);
+    const barGeometry = useMemo(() => {
+      const geometry = new THREE.CylinderGeometry(0.0005, 0.006, barHeight, 32);
+      geometry.translate(0, barHeight / 2, 0);
+      return geometry;
+    }, [barHeight]);
 
-  const barPosition = useMemo(() => {
-    return node.position.clone().normalize().multiplyScalar(earthRadius);
-  }, [node.position, earthRadius]);
+    const barPosition = useMemo(() => {
+      return node.position.clone().normalize().multiplyScalar(earthRadius);
+    }, [node.position, earthRadius]);
 
-  const barQuaternion = useMemo(() => {
-    const up = new THREE.Vector3(0, 1, 0);
-    const axis = new THREE.Vector3().crossVectors(up, barPosition).normalize();
-    const radians = Math.acos(up.dot(barPosition.clone().normalize()));
-    return new THREE.Quaternion().setFromAxisAngle(axis, radians);
-  }, [barPosition]);
+    const barQuaternion = useMemo(() => {
+      const up = new THREE.Vector3(0, 1, 0);
+      const axis = new THREE.Vector3()
+        .crossVectors(up, barPosition)
+        .normalize();
+      const radians = Math.acos(up.dot(barPosition.clone().normalize()));
+      return new THREE.Quaternion().setFromAxisAngle(axis, radians);
+    }, [barPosition]);
 
-  const color = useMemo(() => {
-    const baseColor = new THREE.Color(0x00ff00); // Green base color
-    const hoverColor = new THREE.Color(0xffa500); // Orange hover color
-    return hovered ? hoverColor : baseColor;
-  }, [hovered]);
-
-  return (
-    <group
-      position={barPosition}
-      quaternion={barQuaternion}
-      onPointerOver={(e) => {
+    const handlePointerOver = useCallback(
+      (e: THREE.Event) => {
         e.stopPropagation();
         setHovered(true);
         onHover(node);
-      }}
-      onPointerOut={(e) => {
+      },
+      [node, onHover]
+    );
+
+    const handlePointerOut = useCallback(
+      (e: THREE.Event) => {
         e.stopPropagation();
         setHovered(false);
         onHover(null);
-      }}
-      onClick={(e) => {
+      },
+      [onHover]
+    );
+
+    const handleClick = useCallback(
+      (e: THREE.Event) => {
         e.stopPropagation();
         onClick(node);
-      }}
-    >
-      <mesh geometry={barGeometry}>
-        <meshBasicMaterial
-          color={color}
-          opacity={hovered ? 1 : 0.7}
-          transparent
-        />
-      </mesh>
-    </group>
-  );
-};
+      },
+      [node, onClick]
+    );
+
+    return (
+      <group
+        position={barPosition}
+        quaternion={barQuaternion}
+        onPointerOver={handlePointerOver}
+        onPointerOut={handlePointerOut}
+        onClick={handleClick}
+      >
+        <mesh geometry={barGeometry}>
+          <meshBasicMaterial
+            color={hovered ? 0xffa500 : node.color}
+            opacity={hovered ? 1 : 0.7}
+            transparent
+          />
+        </mesh>
+      </group>
+    );
+  }
+);
+
+Node.displayName = 'Node';
 
 export default NanoRepNodes;
